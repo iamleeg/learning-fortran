eb8059d9216bb73dcc6666e710670b639adc6e34
Graham Lee <leeg@labrary.online>
2019-02-11 19:53:06 +0000

    two-dimensional arrays

---
+++ arrays/twodra.f95
@@ -0,0 +1,19 @@
+program twodra
+  ! two-d arrays: you can go up to 7 dimensions according to the tutorial
+  implicit none
+  integer, dimension(2,3) :: a
+  integer :: row, col, count
+  count = 0
+  do row = 1,2
+     count = 0
+     do col = 1,3
+        count = count + 1
+        a(row,col) = count
+     end do
+  end do
+  do row = 1,2
+     do col = 1,3
+        print *, a(row, col)
+     end do
+  end do
+end program twodra
