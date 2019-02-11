3ebf342d3d38774b7131e6e102c7c8207ea2ac5e
Graham Lee <leeg@labrary.online>
2019-02-11 17:01:38 +0000

    Fix the remaining bugs

--- basics/bug.f95
+++ basics/bug.f95
@@ -1,8 +1,9 @@
   !Exercise 1.3: fix the bugs
 program bug
-  this program is full of errors
+  !this program is full of errors
   real :: a,b,c
+  print *, 'Enter two values to find the sum'
+  read *, b, c
   a = b + c
-  read *,c
   print *,a
-end program simple
+end program bug
