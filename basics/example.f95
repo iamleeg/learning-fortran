5ee37e990a08d0d16e4800c1e2c74a64c6582c9b
Graham Lee <leeg@labrary.online>
2019-02-11 16:52:10 +0000

    Reading from the console

---
+++ basics/example.f95
@@ -0,0 +1,9 @@
+program sum
+  !an example of a program
+  real :: answer,x,y
+  print *, 'Enter two numbers'
+  read *, x
+  read *, y
+  answer = x+y
+  print *, 'The total is ', answer
+end program sum
