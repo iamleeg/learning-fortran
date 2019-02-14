1d62a121d2c15cd94cd0a2f0b47d6c13cf5138e8
Graham Lee <leeg@labrary.online>
2019-02-14 17:16:36 +0000

    Function to compute average of a real array
    The interface block is not in the tutorial, but was needed to make the
    
    function type explicit. I got a warning about an intent mismatch, but in use
    
    the function seemed to work. *shrug*

---
+++ subroutines/average.f95
@@ -0,0 +1,39 @@
+  
+
+program avg
+  implicit none
+  interface
+     function average(count, numbers)
+       integer :: count
+       real, intent(in) :: numbers(:)
+       real average
+     end function average
+  end interface
+  integer :: count, i
+  real, allocatable, dimension(:) :: elements
+
+  print *, "Enter the number of elements"
+  read *, count
+  allocate(elements(count))
+  print *, "Enter each of the ", count, " elements"
+  do i=1,count
+     read *, elements(i)
+  end do
+  print *, "The average is ", average(count, elements)
+  deallocate(elements)
+end program avg
+
+function average(count, numbers)
+  implicit none
+  integer :: count, i
+  real :: sum, average
+  real, dimension(:) :: numbers
+
+  sum = 0.
+  do i = 1,count
+     sum = sum + numbers(i)
+  end do
+  average = sum/count
+end function average
+
+  
